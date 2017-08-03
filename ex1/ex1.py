62b6af7f74832b00a0abed7c44a370e1f8928a6d
Harri Pahirathan <harripahirathan@gmail.com>
2017-08-03 01:44:32 -0400

    Add files via upload

---
+++ ex1/ex1.py
@@ -0,0 +1,30 @@
+1
+impossible
+2
+stack
+stack put(B) move(A) put(N) move(A) put(N) move(A) get(N) get(N) get(B)
+3
+bucket stack queue
+bucket move(B) put(A) move(N) get(A) move(A) put(N) move(A) get(N)
+stack move(B) put(A) move(N) get(A) move(A) put(N) move(A) get(N)
+queue move(B) put(A) move(N) get(A) move(A) put(N) move(A) get(N)
+4
+queue
+queue put(B) put(A) move(N) get(B) put(A) move(N) get(A) get(A) move(A)
+5
+stack
+stack put(B) put(A) move(N) put(A) move(N) get(A) get(A) move(A) get(B)
+6
+queue
+queue put(B) put(A) move(N) put(A) move(N) get(B) get(A) get(A) move(A)
+7
+bucket stack queue
+bucket put(B) move(A) move(N) move(A) move(N) move(A) get(B)
+stack put(B) move(A) move(N) move(A) move(N) move(A) get(B)
+queue put(B) move(A) move(N) move(A) move(N) move(A) get(B)
+8
+stack queue
+stack put(B) put(A) move(N) get(A) get(B) move(A) move(N) move(A)
+queue put(B) put(A) move(N) move(A) get(B) get(A) move(N) move(A)
+9
+impossible
